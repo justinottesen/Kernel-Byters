b88e7d36795da714d9c2dca7fbbc70e220c85353
justinottesen <49543532+justinottesen@users.noreply.github.com>
2022-01-05 22:04:52 -0500

    Update Archon.java

--- src/structured/Archon.java
+++ src/structured/Archon.java
@@ -1,14 +1,26 @@
 package structured;
 import battlecode.common.*;
 public class Archon extends RobotLogic {
+	private static int turnNum = 0;
+	private static int previous = -1;
+	private static int orderValue = 0;
 	private static final RobotType[] buildOrder={
 	    	RobotType.MINER,
 	    	RobotType.BUILDER,
 	    	
 	    	RobotType.SOLDIER
 	};
-	static int buildNum=0;
+	private static int buildNum=0;
+	public void communicate(RobotController rc) throws GameActionException {
+		int commVal = locToComm(rc.getLocation());
+		if (turnNum%4 == orderValue && rc.readSharedArray(0) == previous) {
+			rc.writeSharedArray(0, commVal);
+		} else if (turnNum%4 == orderValue) {
+			orderValue ++;
+		}
+	}
 	public boolean run(RobotController rc) throws GameActionException{
+		turnNum ++;
 		Direction dir = directions[1];
     	if (rc.canBuildRobot(buildOrder[buildNum], dir)) {
             rc.buildRobot(buildOrder[buildNum], dir);
@@ -16,6 +28,7 @@ public class Archon extends RobotLogic {
             if(buildNum>=buildOrder.length)
             	buildNum=0;
         }
+    	previous = rc.readSharedArray(0);
     	return true;
 	}
 }
