f908fa1c67eb5be8d6ecea48da67b1aff920f92e
kernelbyters1 <96930535+kernelbyters1@users.noreply.github.com>
2022-01-06 22:41:23 -0500

    Miner doesn't mine lead deposits to extinction now

--- src/structured/Miner.java
+++ src/structured/Miner.java
@@ -22,7 +22,7 @@ public class Miner extends RobotLogic {
                 while (rc.canMineGold(mineLocation)) {
                     rc.mineGold(mineLocation);
                 }
-                while (rc.canMineLead(mineLocation)) {
+                while (rc.canMineLead(mineLocation)&&rc.senseLead(mineLocation)>1) {
                     rc.mineLead(mineLocation);
                 }
             }
@@ -37,15 +37,7 @@ public class Miner extends RobotLogic {
 			if(m.x==me.x&&m.y==me.y) {
 				m=null;
 			}else { //else move towards the location
-				Direction dir=me.directionTo(m);
-	        	//rc.setIndicatorString("attempting to move "+dir);
-	        	if(rc.canMove(dir)) {
-	        		rc.move(dir);
-	        	}else if(rc.canMove(dir.rotateLeft())) { //if it can't move in the direction, tries to move 45 degrees to the left
-	        		rc.move(dir.rotateLeft());
-	        	}else if(rc.canMove(dir.rotateRight())) {//tries to move 45 degrees to the right
-	        		rc.move(dir.rotateRight());
-	        	}
+				super.pathFind(rc,m);
 			}
 		}else {//if no assigned location
 			//don't move if happily mining ore
@@ -54,34 +46,52 @@ public class Miner extends RobotLogic {
 	                MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
 	                if(rc.canSenseLocation(mineLocation)&&(rc.senseLead(mineLocation)>0||rc.senseGold(mineLocation)>0)){
 	                	rc.setIndicatorString("mining :)");
-	                	return;
+	                	//make sure miner isn't blocking archon
+	                	boolean nextToArchon=false;
+	                	RobotInfo[] archon=rc.senseNearbyRobots(2);
+	                	for(int i=0;i<archon.length&&!nextToArchon;++i) {
+	                		if(archon[i].getType()==RobotType.ARCHON&&archon[i].getMode()==RobotMode.TURRET) {
+	                			nextToArchon=true;
+	                		}
+	                	}
+	                	if(!nextToArchon)
+	                		return;
 	                }
 	            }
 			}
 			//look for nearby deposits without nearby miners
+			MapLocation[] gold=rc.senseNearbyLocationsWithGold(20);
+			MapLocation[] lead=rc.senseNearbyLocationsWithLead(20);
 			RobotInfo[] miners=rc.senseNearbyRobots(20,rc.getTeam());
-			for (int dx = -4; dx <= 4; dx++) {
-	            for (int dy = -4; dy <= 4; dy++) {
-	            	if(dx*dx+dy+dy>2) {
-		                MapLocation searchLocation = new MapLocation(me.x + dx, me.y + dy);
-		                if(rc.canSenseLocation(searchLocation)&&(rc.senseLead(searchLocation)>0||rc.senseGold(searchLocation)>0)){
-		                	//location has ore, check if it has an adjacent miner
-		                	boolean pursue=true;
-		                	for(int i=0;i<miners.length&&pursue;++i) {
-		                		if(miners[i].getType()==RobotType.MINER&&miners[i].getLocation().distanceSquaredTo(searchLocation)<=2) {
-		                			//don't pursue
-		                			pursue=false;
-		                		}
-		                	}
-		                	if(pursue) {
-		                		m=searchLocation;
-		                		move(rc);
-		                		return;
-		                	}
-		                	
-		                }
-	            	}
-	            }
+			for(int j=0;j<gold.length;++j) {
+				//location has gold, check if it has an adjacent miner
+            	boolean pursue=true;
+            	for(int i=0;i<miners.length&&pursue;++i) {
+            		if(miners[i].getType()==RobotType.MINER&&miners[i].getLocation().distanceSquaredTo(gold[j])<=2) {
+            			//don't pursue
+            			pursue=false;
+            		}
+            	}
+            	if(pursue) {
+            		m=gold[j];
+            		move(rc);
+            		return;
+            	}
+			}
+			for(int j=0;j<lead.length;++j) {
+				//location has lead, check if it has an adjacent miner
+            	boolean pursue=true;
+            	for(int i=0;i<miners.length&&pursue;++i) {
+            		if(miners[i].getType()==RobotType.MINER&&miners[i].getLocation().distanceSquaredTo(lead[j])<=2) {
+            			//don't pursue
+            			pursue=false;
+            		}
+            	}
+            	if(pursue) {
+            		m=lead[j];
+            		move(rc);
+            		return;
+            	}
 			}
 			//if moving towards wall or enemy, reevaluate direction
 			if(me.x-4<0&&dir.getDeltaX()<0) {//if can see wall in -x and moving towards it
