d1c27252a60b9e3c920af9f8314d0975f4895b2a
justinottesen <49543532+justinottesen@users.noreply.github.com>
2022-01-09 22:02:56 -0500

    turn num implemented

--- src/structured/Miner.java
+++ src/structured/Miner.java
@@ -2,10 +2,12 @@ package structured;
 import battlecode.common.*;
 
 public class Miner extends RobotLogic {
+	int turnNum = 0;
 	private static Direction dir = directions[rng.nextInt(directions.length)];
 	private static MapLocation assignment=null;
 	@Override
 	public boolean run(RobotController rc) throws GameActionException{
+		turnNum ++;
 		// Try to mine on squares around us.
         mine(rc);
         assignment=super.allAboard(rc);
