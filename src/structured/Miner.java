fb345407ca0f90cca91096a466ae8b8c7d1e8925
kernelbyters1 <96930535+kernelbyters1@users.noreply.github.com>
2022-01-13 14:05:41 -0500

    good wandering now

--- src/structured/Miner.java
+++ src/structured/Miner.java
@@ -5,15 +5,21 @@ public class Miner extends RobotLogic {
 	private static Direction dir = directions[rng.nextInt(directions.length)];
 	private static MapLocation assignment=null;
 	private int commNum=-1;
+	private int role=-1;
 	@Override
 	public boolean run(RobotController rc) throws GameActionException{
+		if(role==-1||(role==0&&rc.getRoundNum()==super.TRANSITIONROUND+1)) {
+			determineRole(rc);
+		}
+		rc.setIndicatorString("role: "+role);
 		// Try to mine on squares around us.
         mine(rc);
-        if(rc.getRoundNum()<super.TRANSITIONROUND) {
-        	//recode basic mining here
+        if(role==0) {
+        	//basic mining
         	move(rc);
         	communicateRubble(rc);
         }else {
+        	//train
 	        assignment=super.allAboard(rc);
 	        if(assignment!=null) {
 	        	chooChoo(rc,assignment);
@@ -24,6 +30,19 @@ public class Miner extends RobotLogic {
         //doesn't need to be in train mode to report enemy archons!
     	commEnemyArchonLoc(rc);
 		return true;
+	}
+	private void determineRole(RobotController rc) throws GameActionException{
+		if(rc.getRoundNum()<super.TRANSITIONROUND) {
+			role=0;
+		}else {
+			Random random=new Random(rc.getID());
+			int i=random.nextInt(10);
+			if(i>3) {
+				role=0;
+			}else {
+				role=1;
+			}
+		}
 	}
 	private void mine(RobotController rc) throws GameActionException{
 		MapLocation[] gold=rc.senseNearbyLocationsWithGold(2);
@@ -45,16 +64,39 @@ public class Miner extends RobotLogic {
 	}
 	//now used for exploration mining
 	private void move(RobotController rc) throws GameActionException{
-		//set destination to a random spot on the map
-		if(assignment==null||rc.getLocation().distanceSquaredTo(assignment)<5) {
-			//tries to go in a unique direction than other miners
-			Random random=new Random(rc.getRoundNum());
-			assignment=new MapLocation(random.nextInt(rc.getMapWidth()),random.nextInt(rc.getMapHeight()));
+		//additionally, run away from enemy units
+		if(!runAway(rc)) {
+			//set destination to a random spot on the map
+			if(assignment==null||rc.getLocation().distanceSquaredTo(assignment)<5) {
+				//generates a pseudorandom location on the map
+				Random random=new Random(rc.getID()/rc.getRoundNum());
+				MapLocation me=rc.getLocation();
+				
+				//make ddx and ddy such that rng will generate a location on the map
+				int ddx=rc.getMapWidth()/2;
+				int ddy=rc.getMapHeight()/2;
+				if(me.x<ddx) {
+					ddx=me.x;
+				}
+				if(me.y<ddy) {
+					ddy=me.y;
+				}
+				if(rc.getMapWidth()-me.x<ddx) {
+					ddx+=ddx-(rc.getMapWidth()-me.x);
+				}
+				if(rc.getMapHeight()-me.y<ddy) {
+					ddy+=ddy-(rc.getMapHeight()-me.y);
+				}
+				int dx=random.nextInt(rc.getMapWidth())-ddx;
+				int dy=random.nextInt(rc.getMapHeight())-ddy;
+				assignment=new MapLocation(me.x+dx,me.y+dy);
+			}
+			int distance=rc.getLocation().distanceSquaredTo(assignment);
+			rc.setIndicatorString("random loc: "+assignment+", distance: "+distance);
+			
+			//choo choo but ignore lead deposits with adjacent miners
+			chooChooIgnore(rc,assignment);
 		}
-		rc.setIndicatorString("random loc: "+assignment);
-		
-		//choo choo but ignore lead deposits with adjacent miners
-		chooChooIgnore(rc,assignment);
 	}
 	//if theres room in the comms, communicates
 	private void communicateRubble(RobotController rc) throws GameActionException{
@@ -64,7 +106,7 @@ public class Miner extends RobotLogic {
 			int locNum=locToComm(me);
 			int rubbleNum=rc.senseRubble(me)/7;
 			int commRubble=locNum+((int)Math.pow(2,12))*rubbleNum;
-			rc.setIndicatorString("comm: "+commRubble+", commNum: "+commNum);
+			//rc.setIndicatorString("comm: "+commRubble+", commNum: "+commNum);
 			rc.writeSharedArray(commNum,commRubble);
 		}else {
 			//if there's an available spot in comms, claim it
@@ -76,5 +118,23 @@ public class Miner extends RobotLogic {
 				}
 			}
 		}
+	}
+	
+	private boolean runAway(RobotController rc) throws GameActionException{
+		RobotInfo[] enemies=rc.senseNearbyRobots(20,rc.getTeam().opponent());
+		for(int i=0;i<enemies.length;++i) {
+			RobotType type=enemies[i].getType();
+			if(type==RobotType.SOLDIER||type==RobotType.WATCHTOWER||type==RobotType.SAGE) {
+				//if it sees an enemy, runs away
+				MapLocation me=rc.getLocation();
+				MapLocation enemyLoc=enemies[i].getLocation();
+				int dx=enemyLoc.x-me.x;
+				int dy=enemyLoc.y-me.y;
+				MapLocation retreatLoc=new MapLocation(me.x-2*dx,me.y-2*dy);
+				super.pathFind(rc,retreatLoc);
+				return true;
+			}
+		}
+		return false;
 	}
 }
