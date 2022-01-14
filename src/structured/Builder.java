582309fc7c8aaae90b7d59af5da07cdcb6119807
kernelbyters1 <96930535+kernelbyters1@users.noreply.github.com>
2022-01-14 14:32:15 -0500

    place watchtowers on the highground

--- src/structured/Builder.java
+++ src/structured/Builder.java
@@ -1,143 +1,138 @@
 package structured;
 import battlecode.common.*;
 public class Builder extends RobotLogic {
-	
-	private static boolean newBorn=true;
-    private static int idFollow=-1;
-    private static MapLocation assignment=null;
+    private MapLocation assignment=null;
+    
+    //if watchTower is false, the builder is a lab builder
+    private RobotType buildType=RobotType.WATCHTOWER;
+    
 	public boolean run(RobotController rc) throws GameActionException{
-		//finds the miner to follow
-		if(newBorn) {
-			findMiner(rc);
-			newBorn=false;
-		}
-
         //repairs nearby buildings
         repair(rc);
         
-        //builds laboratory if necessary
-        if(assignment!=null) {
-        	buildLaboratory(rc);
-        }else
-        //builds watchtower if necessary (also java is whitespace insensitive so you can do else ifs like this lol)
-        if(shouldBuildWatchTower(rc)) {
-        	buildWatchTower(rc);
+        if(assignment==null) {
+	        shouldBuildWatchTower(rc);
+	        shouldBuildLaboratory(rc);
+        }
+        
+        if(assignment!=null&&assignment.distanceSquaredTo(rc.getLocation())!=0) {
+	        build(rc);
         }
         
         shmovement(rc);
+        rc.setIndicatorString("assignment: "+assignment);
         return true;
 	}
-	private boolean findMiner(RobotController rc) throws GameActionException{
-        //searches every nearby robot for the closest miner
-        RobotInfo[] nearby=rc.senseNearbyRobots(20,rc.getTeam());
-        int closest=21;
-        boolean found=false;
-        for(int i=0;i<nearby.length;++i) {
-       		if(nearby[i].getType()==RobotType.MINER&&nearby[i].getLocation().distanceSquaredTo(rc.getLocation())<closest) {
-       			closest=nearby[i].getLocation().distanceSquaredTo(rc.getLocation());
-       			idFollow=nearby[i].getID();
-       			found=true;
-       		}
-       	}
-        //System.out.println("idFollow: "+idFollow);
-        rc.setIndicatorString("following "+idFollow);
-        return found;
-	}
 	private void shmovement(RobotController rc) throws GameActionException{
-		//hopefully, the builder won't get stuck on standing on the place where it should build
-		MapLocation me = rc.getLocation();
-		if(assignment!=null&&(me.x!=assignment.x&&me.y!=assignment.y)) {//if it has somewhere to build a laboratory (and it isn't already on the assigment tile)
-			//move towards assignment
-			//also effectively stays at assignment once there
-			rc.setIndicatorString("assigned location: "+assignment);
-			Direction dir=me.directionTo(assignment);
-			if(rc.canMove(dir)) {
-        		rc.move(dir);
-        	}else if(rc.canMove(dir.rotateLeft())) { //if it can't move in the direction, tries to move 45 degrees to the left
-        		rc.move(dir.rotateLeft());
-        	}else if(rc.canMove(dir.rotateRight())) {//tries to move 45 degrees to the right
-        		rc.move(dir.rotateRight());
-        	}
+		//need to rework
+		
+		//fuck around until it finds an assignment
+		if(assignment==null) {
+			super.randomMovement(rc);
 		}else {
-			//follow the miner
-	        if(!super.follow(rc,idFollow)) {
-	        	//if it can't follow miner, it tries to find a new miner
-	        	if(findMiner(rc)) {
-	        		super.follow(rc,idFollow);
-	        	}else {
-		        	//if it can't, then it moves randomly
-		        	rc.setIndicatorString("miner out of range :(");
-		        	Direction dir = directions[rng.nextInt(directions.length)];
-		            if (rc.canMove(dir)) {
-		                rc.move(dir);
-		            }
-	        	}
+			MapLocation me=rc.getLocation();
+			int distance=me.distanceSquaredTo(assignment);
+			//if it has an assignment, move towards it
+			if(distance!=1||distance!=2) {
+				if(distance==0) {
+					//if its on top of the assignment, move to the lowest rubble tile
+					int lowest=696969;
+					int index=0;
+					for(int i=0;i<directions.length;++i) {
+						MapLocation adjacent=me.add(directions[i]);
+						if(rc.onTheMap(adjacent)&&rc.senseRobotAtLocation(adjacent)==null) {
+							int rubble=rc.senseRubble(adjacent);
+							if(rubble<lowest) {
+								lowest=rubble;
+								index=i;
+							}
+						}
+					}
+					if(rc.canMove(directions[index])) {
+						rc.move(directions[index]);
+					}
+				}else if(distance<=5){
+					//close to assignment, move greedily
+					super.greedy(rc,assignment);
+				}else {
+					//far from assignment, regular pathfind
+					super.pathFind(rc,assignment);
+				}
+			}
+		}
+	}
+	private void shouldBuildWatchTower(RobotController rc) throws GameActionException{
+		//look at a tile and the tiles on the edge of the builder's vision
+		//if the tile has low rubble compared to the tiles center-side of it
+		//tldr: looks for locations that have the high ground
+		MapLocation me = rc.getLocation();
+		
+		//might want to change the condition instead of just "double"
+		//or not, maybe double is fine?
+        if(rc.senseRubble(me)*2<getAvgRimRubble(rc)) {
+        	assignment=me;
+        }
+	}
+	
+	//possible issue: ignores high standard deviation
+	//solution: ultra penalize for low rubble
+	private int getAvgRimRubble(RobotController rc) throws GameActionException{
+		Direction dirToCenter=directionToCenter(rc);
+		Direction dirLeft=dirToCenter.rotateLeft().rotateLeft();
+		int dx=dirLeft.getDeltaX();
+		int dy=dirLeft.getDeltaY();
+		if(dx==0||dy==0) {
+			//non-diagonal
+			dx*=4;
+			dy*=4;
+		}else {
+			//diagonal
+			dx*=3;
+			dy*=3;
+		}
+		//build a semicircle clockwise
+		MapLocation rim=rc.getLocation().translate(dx,dy);
+		if(rc.onTheMap(rim)) {
+			int total=0;
+			//try dirToCenter.rotateRight(), then dirToCenter.rotateRight().rotateRight(), 
+			//then dirToCenter.rotateRight().rotateRight().rotateRight()
+			for(int i=0;i<13;++i) {
+				//add to total
+				int rubble=rc.senseRubble(rim);
+				total+=rc.senseRubble(rim);
+				if(rubble<25) {//penalty for ultra-low rubble
+					total-=(25-rubble);
+				}
+				//move around the rim
+				Direction dirToNextRim=dirToCenter;
+				while(!rc.canSenseLocation(rim.add(dirToNextRim))) {
+					dirToNextRim=dirToNextRim.rotateRight();
+				}
+				rim=rim.add(dirToNextRim);
+			}
+			return total/13;
+		}else {//if the first rim isn't on the map, be lazy for now
+			return 0;
+		}
+	}
+	
+	//assumption: assignment isn't null
+	private boolean build(RobotController rc) throws GameActionException{
+		MapLocation me = rc.getLocation();
+		if(me.distanceSquaredTo(assignment)<6) {
+	       	Direction dir=me.directionTo(assignment);
+	       	if(dir!=Direction.CENTER&&rc.canBuildRobot(buildType,dir)) {
+	       		//note: watchtower placement is so crucial that we cannot place it anywhere but the assignment
+		       	rc.setIndicatorString("trying to build a "+buildType+": "+dir);
+		       	rc.buildRobot(RobotType.WATCHTOWER,dir);
+		       	return true;
 	        }
 		}
-	}
-	private boolean shouldBuildWatchTower(RobotController rc) throws GameActionException{
-		boolean build=false;
-		MapLocation me = rc.getLocation();
-        for (int dx = -2; dx <= 2&&!build; dx++) {
-            for (int dy = -2; dy <= 2&&!build; dy++) {
-                MapLocation searchLocation = new MapLocation(me.x + dx, me.y + dy);
-                if(rc.canSenseLocation(searchLocation))
-                	build=(rc.senseLead(searchLocation)>0);
-            }
-        }
-        return build;
-	}
-	private boolean buildWatchTower(RobotController rc) throws GameActionException{
-       	//scans every available location
-       	//prioritizes building in locations with low lead
-		MapLocation me = rc.getLocation();
-       	int lead=6969;
-       	Direction dir=null;
-        for(int i=0;i<8;++i) {
-       		MapLocation buildLocation=me.add(directions[i]);
-       		if(rc.canBuildRobot(RobotType.WATCHTOWER,directions[i])&&rc.senseLead(buildLocation)<lead) {
-       			//make sure there are no buildings adjacent to buildLocation
-       			boolean adjacent=false;
-       			RobotInfo[] adjacentRobots=rc.senseNearbyRobots(buildLocation,2,rc.getTeam());
-       			for(int j=0;j<adjacentRobots.length&&!adjacent;++j) {
-       				if(adjacentRobots[j].getType().isBuilding())
-       					adjacent=true;
-        		}
-        		if(!adjacent) {
-	        		lead=rc.senseLead(buildLocation);
-	        		dir=directions[i];
-       			}
-       		}
-       	}
-       	if(dir!=null) {
-	       	rc.setIndicatorString("trying to build watchtower "+dir);
-	       	rc.buildRobot(RobotType.WATCHTOWER,dir);
-	       	return true;
-        }
         return false;
 	}
-	//todo: sets assignment
-	private boolean shouldBuildLaboratory(RobotController rc) throws GameActionException{
+	//todo:
+	private void shouldBuildLaboratory(RobotController rc) throws GameActionException{
 		//??
-		return false;
-	}
-	private boolean buildLaboratory(RobotController rc) throws GameActionException{
-		MapLocation me = rc.getLocation();
-		if(assignment.isAdjacentTo(me)) {
-			//build
-			Direction dir=me.directionTo(assignment);
-			if(rc.canBuildRobot(RobotType.LABORATORY,dir)) {
-				rc.setIndicatorString("trying to build laboratory "+dir);
-		       	rc.buildRobot(RobotType.LABORATORY,dir);
-		       	return true;
-			}else {
-				//maybe report back that something is in the way
-				//or maybe improvise and try placing the laboratory somewhere else and notifying the archon
-				return false;
-			}
-		}else {
-			return false;
-		}
 	}
 	private boolean repair(RobotController rc) throws GameActionException{
 		boolean repaired=false;
