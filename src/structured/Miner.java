20f2bc416aa6ebc7d6d12cf445b8918aba839e36
kernelbyters1 <96930535+kernelbyters1@users.noreply.github.com>
2022-01-17 17:02:12 -0500

    zone mining + bad run away

--- src/structured/Miner.java
+++ src/structured/Miner.java
@@ -2,80 +2,54 @@ package structured;
 import battlecode.common.*;
 import java.util.Random;
 public class Miner extends RobotLogic {
-	private static Direction dir = directions[rng.nextInt(directions.length)];
-	private static MapLocation assignment=null;
+	private MapLocation[] zoneCorners= {null};
 	private int commNum=-1;
-	private int role=-1;
+	private int[] zoneLead= {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+	private int[] zoneMiners={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+	private int[] zoneEnemies={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
 	@Override
 	public boolean run(RobotController rc) throws GameActionException{
-		if (rc.readSharedArray(ENEMY_SOLDIER_SEEN) == 0) {
-			RobotInfo[] enemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
-			if (enemies.length > 0) {
-				for (int i = 0; i < enemies.length; i++) {
-					if (enemies[i].type == RobotType.SOLDIER) {
-						rc.writeSharedArray(ENEMY_SOLDIER_SEEN, 1);
-						break;
-					}
-				}
+		//read zone info (lead and miners)
+		if(rc.getRoundNum()%2==1) {
+			readZoneInfo(rc);
+			//only reevaluates assigned zone if the miner has completely scouted its current zone
+			//and it is disatisfied with its current zone
+			//rc.setIndicatorString("c1: "+(numCornersNotSeen()==0)+", c2: "+(!zoneGood(rc,getZone(rc,rc.getLocation()))));
+			if(zoneCorners.length==1||numCornersNotSeen()==0&&!zoneGood(rc,getZone(rc,rc.getLocation()))) {
+				processZoneInfo(rc);
 			}
 		}
-		if(role==-1||(role==0&&rc.getRoundNum()==super.TRANSITIONROUND+1)) {
-			determineRole(rc);
-		}
-		rc.setIndicatorString("role: "+role);
+		
 		// Try to mine on squares around us.
         mine(rc);
-        if(role==0) {
-        	//basic mining
-        	move(rc);
-        	communicateRubble(rc);
+        runAway(rc);
+        //move towards assignment or unocupied nearby lead deposits
+        if(numCornersNotSeen()>0) {
+        	super.chooChooIgnore(rc,zoneCorners[closestCorner(rc)]);
+        	//rc.setIndicatorString("zone: "+getZone(rc,rc.getLocation()));
+        	lookForCorners(rc);
         }else {
-        	//train
-	        assignment=super.allAboard(rc);
-	        if(assignment!=null) {
-	        	chooChoo(rc,assignment);
-	        	commNoEnemyArchon(rc,assignment);
-	        }else
-	        	move(rc);
+        	//if it doesn't have an assignment, just move to the lowest rubble tile
+        	if(zoneCorners.length==1)
+        		super.superGreedy(rc);
+        	else {
+        		//mine
+        		MapLocation random=rc.getLocation().add(directions[rng.nextInt(8)]);
+        		super.chooChoo(rc,random);
+        	}
         }
+        
+        // Try to mine again (just in case we moved within range of a deposit)
+        mine(rc);
+        
+        
+        
         //doesn't need to be in train mode to report enemy archons!
     	commEnemyArchonLoc(rc);
-		/*
-		if (rc.readSharedArray(LEAD_INCOME) == 0) {
-			if (rc.senseNearbyLocationsWithLead(-1, 2).length < 3 && rc.getRobotCount() > rc.getArchonCount()*5) {
-				RobotInfo[] nearby = rc.senseNearbyRobots();
-				int visibleMinerCount = 0;
-				for (int j = 0; j < 2; j++) {
-					for (int i = 0; i < nearby.length; i++) {
-						if (j == 0 && nearby[i].team == rc.getTeam().opponent()) {
-							break;
-						}
-						if (j == 0 && nearby[i].type == RobotType.MINER) {
-							visibleMinerCount ++;
-						}
-						if (j == 1 && nearby[i].type == RobotType.ARCHON && visibleMinerCount > 1 && rc.getLocation().distanceSquaredTo(nearby[i].location) < 6) {
-							rc.disintegrate();
-							return true;
-						}
-					}
-				}
-			}
-		}*/
-		rc.writeSharedArray(MINER_NUMBER_COUNTER, rc.readSharedArray(MINER_NUMBER_COUNTER)+1);
+    	if(rc.getRoundNum()%2==0) {
+    		reportZone(rc);
+    	}
 		return true;
-	}
-	private void determineRole(RobotController rc) throws GameActionException{
-		if(rc.getRoundNum()<super.TRANSITIONROUND) {
-			role=0;
-		}else {
-			Random random=new Random(rc.getID());
-			int i=random.nextInt(10);
-			if(i>2) {
-				role=0;
-			}else {
-				role=1;
-			}
-		}
 	}
 	private void mine(RobotController rc) throws GameActionException{
 		int minedLead = 0;
@@ -98,42 +72,113 @@ public class Miner extends RobotLogic {
         }
 		rc.writeSharedArray(MINER_LEAD_COUNTER, rc.readSharedArray(MINER_LEAD_COUNTER)+minedLead);
 	}
-	//now used for exploration mining
-	private void move(RobotController rc) throws GameActionException{
-		//additionally, run away from enemy units
-		if(!runAway(rc)) {
-			//set destination to a random spot on the map
-			if(assignment==null||rc.getLocation().distanceSquaredTo(assignment)<5) {
-				//generates a pseudorandom location on the map
-				Random random=new Random(rc.getID()/rc.getRoundNum());
-				MapLocation me=rc.getLocation();
-				
-				//make ddx and ddy such that rng will generate a location on the map
-				int ddx=rc.getMapWidth()/2;
-				int ddy=rc.getMapHeight()/2;
-				if(me.x<ddx) {
-					ddx=me.x;
-				}
-				if(me.y<ddy) {
-					ddy=me.y;
-				}
-				if(rc.getMapWidth()-me.x<ddx) {
-					ddx+=ddx-(rc.getMapWidth()-me.x);
-				}
-				if(rc.getMapHeight()-me.y<ddy) {
-					ddy+=ddy-(rc.getMapHeight()-me.y);
-				}
-				int dx=random.nextInt(rc.getMapWidth())-ddx;
-				int dy=random.nextInt(rc.getMapHeight())-ddy;
-				assignment=new MapLocation(me.x+dx,me.y+dy);
+	
+	//sets zoneLead and zoneMiners based on the values in comm
+	private void readZoneInfo(RobotController rc) throws GameActionException{
+		for(int i=53;i>37;--i) {
+			int index=53-i;
+			int comm=rc.readSharedArray(i);
+			if(comm!=0) {
+				//make sure these get the final variables from robotlogic
+				int commMinEnemy=(comm%enemyMinMod)/roundMod;
+				int commMinLead=(comm%leadMinMod)/enemyMinMod;
+	    		int commNumMiners=(comm%allyMinerMod)/leadMinMod;
+	    		zoneLead[index]=commMinLead;
+	    		zoneMiners[index]=commNumMiners;
+	    		zoneEnemies[index]=commMinEnemy;
 			}
-			int distance=rc.getLocation().distanceSquaredTo(assignment);
-			rc.setIndicatorString("random loc: "+assignment+", distance: "+distance);
-			
-			//choo choo but ignore lead deposits with adjacent miners
-			chooChooIgnore(rc,assignment);
 		}
 	}
+	
+	//evaluates destination based on the zoneInfo
+	private void processZoneInfo(RobotController rc) throws GameActionException{
+		//starting order favors middle zones
+		int[] indecies= {8,7,9,6,10,5,11,4,12,3,13,2,14,1,15,0};
+		//shuffles the indecies to make the order it checks the zones in random
+		shuffle(rc,indecies);
+		
+		//for now, choose the first suitable zone it comes across (that is better than its current one)
+		for(int i=0;i<15;++i) {
+			if(zoneGood(rc,indecies[i])) {
+				//assignment=super.getZoneCenter(rc,indecies[i]);
+				zoneCorners=super.getZone(rc,indecies[i]);
+				rc.setIndicatorString("zone: "+indecies[i]);
+				return;
+			}
+		}
+	}
+	
+	//shuffles an ordered set of indecies to provide a pseudorandom order
+	private void shuffle(RobotController rc,int[] indecies) throws GameActionException{
+		Random random=new Random(rc.getID()/rc.getRoundNum());
+		//can shuffle less for less bytecode
+		for(int i=0;i<10;++i) {
+			int index1=random.nextInt(16);
+			int index2=random.nextInt(16);
+			//probably more bytecode efficient to leave out the if statement
+			//if(index1!=index2) {
+				//swap
+				int temp=indecies[index1];
+				indecies[index1]=indecies[index2];
+				indecies[index2]=temp;
+			//}
+		}
+		
+	}
+	
+	private boolean zoneGood(RobotController rc,int zone) throws GameActionException{
+		//prioritize exploration and lead
+		//deprioritize enemies
+		
+		//lets say, saturation is 1 miner per 2-3 lead deposit
+		
+		//don't assign a zone with enemies
+		//hopefully not all zones will be occupied with enemies
+		if(zoneEnemies[zone]>0) {
+			return false;
+		}
+		//rc.setIndicatorString("c1: "+(zoneMiners[zone]==-1)+" c2: "+(zoneMiners[zone]==0&&zoneLead[zone]>0)+" c3: "+(zoneMiners[zone]>0&&(zoneLead[zone]/zoneMiners[zone])>2));
+		//zone is unexplored or     zone is undersaturated
+		if(zoneMiners[zone]==-1||(zoneMiners[zone]==0&&zoneLead[zone]>0)||(zoneMiners[zone]>0&&(zoneLead[zone]/zoneMiners[zone])>2)) {
+			return true;
+		}
+		return false;
+	}
+	
+	//checks off from the corners array any corners the miner sees
+	private void lookForCorners(RobotController rc) throws GameActionException{
+		MapLocation me=rc.getLocation();
+		for(int i=0;i<zoneCorners.length;++i) {
+			if(zoneCorners[i]!=null&&me.distanceSquaredTo(zoneCorners[i])<21) {
+				zoneCorners[i]=null;
+			}
+		}
+	}
+	
+	//returns the number of corners the miner has yet to see
+	private int numCornersNotSeen() {
+		int counter=0;
+		for(int i=0;i<zoneCorners.length;++i) {
+			if(zoneCorners[i]!=null)
+				counter++;
+		}
+		return counter;
+	}
+	
+	//returns the index of the corner that is closest to the miner's current location
+	private int closestCorner(RobotController rc) throws GameActionException{
+		int closest=696969;
+		int index=-1;
+		MapLocation me=rc.getLocation();
+		for(int i=0;i<zoneCorners.length;++i) {
+			if(zoneCorners[i]!=null&&me.distanceSquaredTo(zoneCorners[i])<closest) {
+				closest=me.distanceSquaredTo(zoneCorners[i]);
+				index=i;
+			}
+		}
+		return index;
+	}
+	
 	//if theres room in the comms, communicates
 	private void communicateRubble(RobotController rc) throws GameActionException{
 		if(commNum>=0) {
@@ -168,6 +213,7 @@ public class Miner extends RobotLogic {
 				int dy=enemyLoc.y-me.y;
 				MapLocation retreatLoc=new MapLocation(me.x-2*dx,me.y-2*dy);
 				super.pathFind(rc,retreatLoc);
+				processZoneInfo(rc);
 				return true;
 			}
 		}
